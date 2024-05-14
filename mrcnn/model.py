e6176726755738c7f1817636c32cbed866315a0d
knee-son <83338313+knee-son@users.noreply.github.com>
2024-05-15 05:28:59 +0800

    Update model.py

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -17,11 +17,16 @@ from collections import OrderedDict
 import multiprocessing
 import numpy as np
 import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import backend as K
-from tensorflow.keras import layers as KL
-from tensorflow.keras import layers as KE
-from tensorflow.keras import models as KM
+import keras
+import keras.backend as K
+import keras.layers as KL
+import keras.layers as KE
+import keras.models as KM
+# from tensorflow import keras
+# from tensorflow.keras import backend as K
+# from tensorflow.keras import layers as KL
+# from tensorflow.keras import layers as KE
+# from tensorflow.keras import models as KM
 
 from mrcnn import utils
 
@@ -2136,7 +2141,8 @@ class MaskRCNN():
 
         if by_name:
             saving.load_weights_from_hdf5_group_by_name(f, layers)
-          # hdf5_format.load_weights_from_hdf5_group(f, layers)
+            # saving.load_weights_from_hdf5_group(f, layers)
+            # hdf5_format.load_weights_from_hdf5_group(f, layers)
         else:
             saving.load_weights_from_hdf5_group(f, layers)
         if hasattr(f, 'close'):
