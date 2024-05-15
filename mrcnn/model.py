ad9d3f80e34e08a62ef150a5f530a6f592549749
knee-son <83338313+knee-son@users.noreply.github.com>
2024-05-15 08:40:07 +0800

    Update model.py

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -23,6 +23,8 @@ import keras.layers as KL
 import keras.layers as KE
 import keras.models as KM
 
+tf.compat.v1.disable_v2_behavior()
+
 from mrcnn import utils
 
 # Requires TensorFlow 1.3+ and Keras 2.0.8+.
