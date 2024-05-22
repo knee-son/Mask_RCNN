bf0e28db0ba016c7d564e473b900450dc7fc6039
knee-son <83338313+knee-son@users.noreply.github.com>
2024-05-22 19:05:34 +0800

    Update model.py

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -5,6 +5,7 @@ The main Mask R-CNN model implementation.
 Copyright (c) 2017 Matterport, Inc.
 Licensed under the MIT License (see LICENSE for details)
 Written by Waleed Abdulla
+Revisions by Charniel Duterte
 """
 
 import os
